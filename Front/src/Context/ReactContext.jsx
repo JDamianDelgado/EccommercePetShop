5132a3e1631243f6dfa292c988557f43e698028a
JDamianDelgado <joakodelgado2013@gmail.com>
2025-03-21 20:01:45 -0300

    estructura de eccommerce

---
+++ Front/src/Context/ReactContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+export const UserContext = createContext({
+  currentUser: null,
+  loginUser: async () => {},
+});
+
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const navigate = useNavigate();
+  const loginUser = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/users/login",
+        data
+      );
+      if (response.data === 200) {
+        setCurrentUser(response.data);
+        return true;
+      }
+    } catch (error) {
+      alert(error.response.data || error.message);
+      return false;
+    }
+  };
+
+  const logOut = () => {
+    setCurrentUser(null);
+    alert("Se cerro sesion");
+    navigate("/Home");
+  };
+  const values = {
+    currentUser,
+    setCurrentUser,
+    logOut,
+    loginUser,
+  };
+
+  UserProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  return (
+    <UserContext.Provider value={values}> {children}</UserContext.Provider>
+  );
+};
